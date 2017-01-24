0d3e9d2c115c2b62522ca8e8fb151d2ce489e206
Austin Thompson <stoodachance@gmail.com>
2017-01-24 16:08:05 +0000

    Added CSCI328-Public and followed Lecturcize

---
+++ Lecturcize1/Dog.java
@@ -0,0 +1,13 @@
+    public class Dog{
+    
+        // State....
+        private String m_name;
+        
+        // Behavior....
+        public Person(String name){
+            m_name = name;
+        }
+        public void bark (){
+            System.out.pritln(m_name);
+        }
+    }
